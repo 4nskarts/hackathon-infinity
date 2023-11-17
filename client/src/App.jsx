dddad899b95eeafed9e3838a4ce4f0c415990618
4nskarts <anskarts@ArchLinux.localdomain>
2023-11-17 09:10:55 +0100

    finished the home page (basic)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,22 @@ import LoginPage from "./pages/LoginPage";
 import Home from "./pages/HomePage/Home";
 /* import LoginPage from "./pages/LoginPage/LoginPage"; */
 
-const User = {
-  name: "John Doe",
-  profilePicture:
-    "https://i.pinimg.com/originals/e2/c5/bd/e2c5bdd206a4cb7b118e3a72f1a53d13.jpg",
-};
+// This is static now, don't remove it
 
-const Issue = {
-  title: "Example Issue",
-  description:
-    "This is an example issue description. It might be a bit long, but that's okay. ut molestiae est Cupiditate dignissimos repellendus consequatur omnis. Harum ipsam porro. Voluptatibus doloremque possimus quas iste nesciunt.",
-  tags: ["bug", "feature", "help-wanted"],
-  postedTime: "2023-01-01T12:34:56", // Replace with the actual posted time
-};
+  const users = Array.from({ length: 10 }, (_, index) => ({
+    name: `dolor voluptate eius ${index + 1}`,
+    profilePicture: `https://i.pinimg.com/originals/cf/d0/e6/cfd0e65ab7aafae09ad428810da84609.jpg`,
+  }));
+
+  // Example data for 10 issues
+  const issues = Array.from({ length: 10 }, (_, index) => ({
+    title: `Consequatur sit hic. ${index + 1}`,
+    description: `Velit qui corrupti necessitatibus earum consequatur ea deleniti ipsam. Distinctio  ${
+      index + 1
+    }`,
+    postedTime: "2023-01-01T12:00:00Z",
+    tags: [`#tag${index + 1}`, `#tag${index + 2}`],
+  }));
 
 function App() {
   return (
@@ -26,7 +29,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<Home user={User} issue={Issue} />} />
+        <Route path="/home" element={<Home users={users} issues={issues} />} />
         {/* <Route path="/login/company" element={<LoginPageCompany />} />
         <Route path="/login/employee" element={<LoginPageEmployee />} /> */}
       </Routes>
--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,55 +1,63 @@
 // Card.js
 
 import React from "react";
-import Navbar from "../../components/Navbar";
 import NavbarHome from "../../components/NavBarHome";
+import "./Card.css";
 
-const Home = ({ user, issue }) => {
+const Home = ({ users, issues }) => {
   const formatDate = (date) => {
     // Implement your own date formatting logic here
     // This is just a basic example
     return new Date(date).toLocaleString();
   };
+  console.log(issues);
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="min-h-screen mt-16">
       <NavbarHome />
-      <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-        <div className="flex items-center px-6 py-4">
-          <img
-            src={user.profilePicture}
-            alt="User Profile"
-            className="h-12 w-12 rounded-full mr-4"
-          />
-          <div>
-            <div className="text-xl font-semibold text-gray-800">
-              {user.name}
+      <div className="grid grid-cols-1 md:grid-cols-2 bg-gray-200 lg:grid-cols-4 gap-0 p-8">
+        {issues.map((issue, index) => (
+          <div
+            key={index}
+            className="max-w-sm mx-2 my-8 bg-white shadow-lg rounded-lg overflow-hidden"
+          >
+            <div className="flex items-center px-6 py-4">
+              <img
+                src={users[index].profilePicture}
+                alt="User Profile"
+                className="h-12 w-12 rounded-full mr-4"
+              />
+              <div>
+                <div className="text-xl font-semibold text-gray-800">
+                  {users[index].name}
+                </div>
+                <p className="text-gray-600 text-sm">
+                  {formatDate(issue.postedTime)}
+                </p>
+              </div>
             </div>
-            <p className="text-gray-600 text-sm">
-              {formatDate(issue.postedTime)}
-            </p>
+            <hr className="border-t" />
+            <div className="px-6 py-4">
+              <h2 className="font-bold text-xl mb-2">{issue.title}</h2>
+              <p className="text-gray-700 text-base mb-4 overflow-hidden overflow-ellipsis h-16 line-clamp-4">
+                {issue.description}
+              </p>
+              <div className="mb-4 flex justify-end">
+                {issue.tags.map((tag, tagIndex) => (
+                  <span
+                    key={tagIndex}
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <button className="bg-[#FFD500] text-black py-1 px-4 mt-4 rounded-lg">
+                Answer
+              </button>
+            </div>
           </div>
-        </div>
-        <hr className="border-t" />
-        <div className="px-6 py-4">
-          <h2 className="font-bold text-xl mb-2">{issue.title}</h2>
-          <p className="text-gray-700 text-base mb-4 overflow-hidden overflow-ellipsis h-16">
-            {issue.description}
-          </p>
-          <div className="mb-4">
-            {issue.tags.map((tag, index) => (
-              <span
-                key={index}
-                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-          <button className="bg-[#FFD500] text-black py-1 px-4 mt-4 rounded-lg">
-            Answer
-          </button>
-        </div>
+        ))}
       </div>
     </div>
   );
