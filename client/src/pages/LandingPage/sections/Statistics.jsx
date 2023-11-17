8d7bee4095053f506113a4f9a5bf143ad943b3d5
ZakLr <lourghizakaria@gmail.com>
2023-11-17 06:39:32 +0100

    added strategies, image assets, updated testimonials

--- client/src/pages/LandingPage/sections/Statistics.jsx
+++ client/src/pages/LandingPage/sections/Statistics.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import Button from "../../../components/PrimaryButton";
 
 function Statistics () {
-    return <section></section>
+    return <section className="min-h-[80vh] flex justify-around items-center">
+        <div>
+            <div className="text-h3">Knowledge Growth:</div>
+            <div className="text-left text-h5 pb-6 text-[#00000099]">Our platform has contributed to a 40% rise in shared knowledge,<br/> fostering a culture of continuous learning and expertise exchange among team members.</div>
+            <Button text={"Learn more"} />
+        </div>
+        <div>
+            <div>
+                <div className="text-h3 font-semibold text-blue p-4">+26</div>
+                <div className="text-center text-h5 font-medium">Company Enrolled</div>
+            </div>
+            <div>
+                <div className="text-h3 font-semibold text-blue p-4">+2300</div>
+                <div className="text-center text-h5 font-medium">Person Engaged</div>
+            </div>
+            <div>
+                <div className="text-h3 font-semibold text-blue p-4">+48000</div>
+                <div className="text-center text-h5 font-medium">Issues</div>
+            </div>
+            <div>
+                <div className="text-h3 font-semibold text-blue p-4">+34000</div>
+                <div className="text-center text-h5 font-medium">Issues Solved</div>
+            </div>
+        </div>
+    </section>
 }
 export default Statistics;
--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,11 @@
 function LoginPage() {
   return (
-      <main className="flex justify-center items-center h-screen">
-      <div className="">
-        <div>qsd</div>
+      <main className="flex justify-center items-center h-screen ">
+      <div className="bg-slate-400 w-[60vw] h-[70vh] flex justify-center items-center">
+        <div className="text-center text-xl">Login</div>
+        <div>
+          
+        </div>
           </div>
     </main>
   )
