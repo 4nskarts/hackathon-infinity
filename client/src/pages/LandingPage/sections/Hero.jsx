5776173cf79c8b759ac8c9763d8d404e4f55e8a0
4nskarts <anskarts@ArchLinux.localdomain>
2023-11-17 04:11:59 +0100

    refactored the hero component and added responsiveness

--- client/src/pages/LandingPage/sections/Hero.jsx
+++ client/src/pages/LandingPage/sections/Hero.jsx
@@ -3,30 +3,27 @@ import SecondaryButton from "../../../components/SecondaryButton";
 
 function Hero() {
   return (
-    <section className="h-screen pt-16 flex flex-row px-[12%] justify-between items-center">
-      <div className="flex flex-col py-14 h-[500px] w-[457px] justify-between">
-        <h1 className="text-h2 font-quicksand font-bold">
+    <section className="h-screen pt-16 flex flex-col md:flex-row px-12 justify-around items-center">
+      <div className="flex flex-col md:w-[457px] justify-between">
+        <h1 className="text-5xl text-center md:text-left md:text-5xl lg:text-6xl font-quicksand font-bold mb-6 leading-tight">
           Knowledge
           <br />
           Sharing
           <br />
           for Success
         </h1>
-        <p>
+        <p className="text-xl text-center md:text-left md:text-base mb-8">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
           corporis veniam harum? Tenetur ullam veniam
         </p>
-        <div className="flex flex-row w-full h-14 justify-between gap-14 ">
-          <button className="py-[12px] px-[35px] rounded-lg bg-black text-white">
-            Start Free Trial
-          </button>
-          <button className="py-[12px] px-[35px] rounded-lg bg-transparent text-black border-solid border-2 border-black">
-            For Your Company
-          </button>
+        <div className="flex flex-col md:flex-row gap-4">
+          <PrimaryButton text="Start Free Trial" />
+          <SecondaryButton text="For Your Company" />
         </div>
       </div>
-      <div className="bg-white w-[360px] h-[360px]"></div>
+      <div className="hidden md:block bg-black rounded-lg w-full md:w-[457px] h-[457px]"></div>
     </section>
   );
 }
+
 export default Hero;
