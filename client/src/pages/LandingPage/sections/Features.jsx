149e8213c2165ff912290432543b22cf66c8bfd9
4nskarts <an.mokhtari@esi-sba.dz>
2023-11-18 10:51:07 +0100

    last commit again lol

--- client/src/pages/LandingPage/sections/Features.jsx
+++ client/src/pages/LandingPage/sections/Features.jsx
@@ -14,11 +14,18 @@ function FeatureSection({ title, description, buttonText, reversed = false }) {
         <img
           src={reversed ? HandshakeIcon : TopIcon}
           className="h-screen border-0 items-center md:w-1/2 "
+          alt="Icon"
         ></img>
         {/* Right Section */}
-        <div className="flex max-w-lg m-auto flex-col  items-start">
-          <div className="text-start font-semibold text-h4 ">{title}</div>
-          <div className="my-6 text-start items-start ">{description}</div>
+        <div className="flex max-w-lg gap-2 m-auto flex-col items-start">
+          <div className="text-start  font-semibold text-h4 ">{title}</div>
+          <ul className="my-6  text-start list-disc list-inside">
+            {description.map((point, index) => (
+              <li className="mt-2" key={index}>
+                {point}
+              </li>
+            ))}
+          </ul>
           <ThirdButton text={buttonText} />
         </div>
       </div>
@@ -27,20 +34,35 @@ function FeatureSection({ title, description, buttonText, reversed = false }) {
 }
 
 function Features() {
+  const featureData = [
+    {
+      title: "Enhance the productivity in your enterprise",
+      description: [
+        "Post your issues and tap into the collective wisdom of the team",
+        "We are committed to a customer-centric approach, tailoring knowledge-sharing solutions to meet the unique needs of your organization.",
+        "We foster a continuous learning culture within your company, where knowledge is shared and acquired organically.",
+      ],
+      buttonText: "Learn more",
+      reversed: false,
+    },
+    {
+      title:
+        "Synergize Solutions: Empower Your Team with Collaborative Mastery",
+      description: [
+        "Post your issues and tap into the collective wisdom of the team.",
+        "We provide an innovative platform for seamless knowledge sharing within your organization.",
+        "We are adaptable to the evolving needs of your business and scalable to accommodate growth.",
+      ],
+      buttonText: "Learn more",
+      reversed: true,
+    },
+  ];
+
   return (
     <div id="features">
-      <FeatureSection
-        title="Enhance the productivity in you entreprise"
-        description="Post your issues and tap into the collective wisdom of the team. This is lorem ipsum Post your issues and tap into the collective wisdom of the team"
-        buttonText="Learn more"
-        reversed={false}
-      />
-      <FeatureSection
-        title="Synergize Solutions: Empower Your Team with Collaborative Mastery"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        buttonText="Learn more"
-        reversed={true}
-      />
+      {featureData.map((feature, index) => (
+        <FeatureSection key={index} {...feature} />
+      ))}
     </div>
   );
 }
